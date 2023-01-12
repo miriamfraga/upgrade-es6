7736ad5505bfdaf8198dd83b09cbde62d83eaf27
“miriamfraga” <fragamr@outlook.com>
2023-01-12 21:41:13 +0100

    iteracion7

---
+++ ejercicio7.js
@@ -0,0 +1,40 @@
+'use strict';
+// 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+// los alumnos usando la función .reduce().
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const sumExams = exams.reduce((cont, exam) => cont + exam.score, 0);
+console.log(sumExams);
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+// alumnos que esten aprobados usando la función .reduce().
+
+const passed = exams.filter(({score}) => score >= 5 );
+ console.log(passed);
+const sumPassed= passed.reduce((acc, exam) => acc + exam.score, 0);
+
+ console.log(sumPassed);
+//  console.log(aprobados);
+
+ const aprobados = exams.reduce((acc, exam)=> acc + exam.score);
+ console.log('aprobados', aprobados);
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce()
+
+const average = exams.reduce((acc, exam) => acc + exam.score / exams.length, 0);
+ console.log(average);
+    
+
+
